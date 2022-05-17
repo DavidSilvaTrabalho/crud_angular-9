06cd7ea6d432448fe2e4d4dbfa99d4898b946608
David Silva <davidemaildetrabalho@gmail.com>
2022-05-16 21:31:34 -0300

    create_sidenav

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,21 +7,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HeaderComponent } from './components/header/header.component';
 
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list'
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { FooterComponent } from './components/footer/footer.component';
+import { NavComponent } from './components/nav/nav.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatListModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
